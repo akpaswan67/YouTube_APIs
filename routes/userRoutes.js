79b85f1032464551e410e425373eb529bcf9666e
akpaswan67 <arunkumarpaswn38@gmail.com>
2025-03-30 12:03:36 +0530

    user login successfully!

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 
 import User from "../models/userModel.js";
 import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
+router.post("/register", async (req, res) => {
 	try {
 	  console.log("request coming");
 	  const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -40,6 +40,55 @@ router.post("/signup", async (req, res) => {
 		.json({ error: "something went wrong", message: error.message });
 	}
   });
+
+  router.post("/login", async (req, res) => {
+	try {
+	  const existingUser = await User.findOne({ email: req.body.email });
+  
+	  if (!existingUser) {
+		return res.status(404).json({ message: "User not found" });
+	  }
+  
+	  const isValid = await bcrypt.compare(
+		req.body.password,
+		existingUser.password
+	  );
+  
+	  if (!isValid) {
+		return res.status(500).json({ message: "Invalid credentials" });
+	  }
+  
+	  const token = jwt.sign(
+		{
+		  _id: existingUser._id,
+		  channelName: existingUser.channelName,
+		  email: existingUser.email,
+		  phone: existingUser.phone,
+		  logoId: existingUser.logoId,
+		},
+		process.env.JWT_TOKEN,
+		{ expiresIn: "10d" }
+	  );
+  
+	  res.status(200).json({
+		  _id: existingUser._id,
+		  channelName: existingUser.channelName,
+		  email: existingUser.email,
+		  phone: existingUser.phone,  
+		  logoId: existingUser.logoId,
+		  logoUrl:existingUser.logoUrl,
+		  token:token,
+		  subscribers:existingUser.subscribers,
+		  subscribedChannels:existingUser.subscribedChannels
+	  })
+	} catch (error) {
+	  console.log(error);
+	  res
+		.status(500)
+		.json({ error: "something went wrong", message: error.message });
+	}
+  });
+  
 
 
 
